e6f31eddbdbe853d3909233f40204b6c1b93a96c
Tyler Ruff <hello@blazed.space>
2022-07-19 23:19:35 -0400

    BLZ

--- script.js
+++ script.js
@@ -102,3 +102,19 @@ function render(){
   requestAnimationFrame(render);
 }
 requestAnimationFrame(render);
+
+let fullscreen;
+let fsEnter = document.getElementById('fullscr');
+fsEnter.addEventListener('click', function (e) {
+    e.preventDefault();
+    if (!fullscreen) {
+        fullscreen = true;
+        document.documentElement.requestFullscreen();
+        fsEnter.innerHTML = "Exit Fullscreen";
+    }
+    else {
+        fullscreen = false;
+        document.exitFullscreen();
+        fsEnter.innerHTML = "Go Fullscreen";
+    }
+});
